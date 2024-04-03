8c51dafd4e50bb507de79d28273b88dbf7b561f9
Alucard1234577432 <165908925+Alucard1234577432@users.noreply.github.com>
2024-04-03 15:13:07 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['+2349161395165']
+global.ownername = "丂卂Ꮆ乇"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
